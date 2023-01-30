e62052af3a71256021a9274554ef5cd04ce63fe7
KFarrow11 <K.Farrow@uea.ac.uk>
2023-01-30 17:35:01 +0000

    calculate the upper and lower 95% confidence interval

--- scripts/Introduction_to_Stats.R
+++ scripts/Introduction_to_Stats.R
@@ -95,3 +95,23 @@ difference_summary <- darwin_wide %>%
 
 difference_summary %>% 
   mutate(se= sd/sqrt(n))
+
+#Create a sequence of 100 equally spaced numbers between -4 and 4
+x <-seq(-4, 4, length=100)
+
+#create a vector of values that shows the height of the probability distribution
+#for each value in x
+y <-dnorm(x)
+
+#plot x and y as a scatterplot with connected lines (type = "l") and add
+#an x-axis with custom labels
+plot(x,y, type = "l", lwd = 2, axes = FALSE, xlab = "", ylab = "")
+axis(1, at = -3:3, labels =c("-3s", "-2s", "-1s", "mean", "1s", "2s", "3s"))
+
+# work out the 95% confidence interval range of our estimated mean
+lowerCI <- 2.62-(2*1.22)
+upperCI <- 2.62+(2*1.22)
+
+lowerCI
+upperCI
+
